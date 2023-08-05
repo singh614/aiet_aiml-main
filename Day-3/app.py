fea476b46bfce315aeb54fd02cc26934ff46e8ce
Abhinandan Singh <singhabhinandan614@gmail.com>
2023-08-05 10:32:28 +0530

    Added Chat Bot

--- Day-3/app.py
+++ Day-3/app.py
@@ -1,15 +1,54 @@
 from flask import Flask, render_template, request
+from nltk.chat.util import Chat, reflections
+
+qn1 = r'how are you'  # raw string (more f-string, b-string...)
+ans1 = [
+    'all well',
+    'I am good',    
+    'awesome' 
+]
+
+qn2 = r'(.*)what (.*)do(.*)'
+ans2 = [    
+    'I can reply to your queries',    
+    'I am here to answer your questions',    
+    'I can chat with you'
+]
+
+qn3 = r'(.*)your name'
+ans3 = [    
+    'my name is chatty',    
+    'I am chatty'
+]
+
+qn4 = r'(.*)mausam(.*)ba[a]*rish'  # aaj mausam kaisa hai, kya baarish hogi?
+ans4 = [    
+    'it looks it will rain today',    
+    'baarish ka mausam hai',  
+    'baarish ho sakti hai mausam kharab hai'
+]
+
+# Pairing qns with ans 
+qa_pair = [
+    (qn1, ans1),
+    (qn2, ans2),
+    (qn3, ans3),
+    (qn4, ans4),
+]
+chatbot = Chat(qa_pair)
 
 app = Flask(__name__)
 
 @app.route('/')
 
 def home():
-
+    global chatbot
     query = request.args.get('Query')
     # print(query)
     if query!=None:
-        response = 'We got query'
+        response = chatbot.respond(query) # respond with stored 
+        if response == None:
+            response = 'Sorry, I am not sure'
     else:
         response = ''
 
